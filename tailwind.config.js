ac3fbd9979d21e0d8ee0f108a383e1056fe6d2e2
Mohd Bilal <bilalmail7054@gmail.com>
2023-11-14 23:31:48 +0530

    added footer component from daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -83,20 +83,18 @@ export default {
     },
   },
   },
-  plugins: [
-    plugin(( { addUtilities, theme }) => {
-      addUtilities({
-        ".fade-up": {
-          transition:
-          "transform 1s cubic-bezier(0.64, 0.04, 0.26, 0.87), opacity 0.8s cubic-bezier(0.64, 0.04, 0.26, 0.87)",
-          opacity: theme("opacity.0"),
-          transform: "translate3d(0, 2rem, 0)",
-        },
-        ".faded": {
-          opacity: theme("opacity.100"),
-          transform: "translate3d(0, 0, 0)",
-        },
-      });
-    }) 
+  plugins: [ 
+    require("daisyui"),
   ],
+  daisyui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: false, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+
 }
